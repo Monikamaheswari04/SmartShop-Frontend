5eeee7c154440a8d82c5dd1603173893affc2aa1
MONIKA A D <admonikamaheswari@gmail.com>
2025-11-17 22:07:11 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
-
-
 export default function Footer() {
   return (
-    <footer className="bg-white dark:bg-gray-800 border-t dark:border-gray-700 p-4 mt-4 text-center text-gray-500 dark:text-gray-400">
+    <footer className="bg-white dark:bg-gray-800 border-t dark:border-gray-700 px-4 py-6 sm:p-4 mt-4 text-center text-sm sm:text-base text-gray-500 dark:text-gray-400">
       &copy; 2025 ProU Store. All rights reserved.
     </footer>
   );
